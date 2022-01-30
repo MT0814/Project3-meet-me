9ed33466d75cfcbf2e3d1a2da55dc7def6997882
MT0814 <millie_tsai@me.com>
2022-01-30 10:32:03 -0800

    updated localhost to insta1

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/insta',
+  process.env.MONGODB_URI || 'mongodb://localhost/insta1',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
